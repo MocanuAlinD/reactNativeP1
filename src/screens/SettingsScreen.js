045a696e03258ca992984456606910eac7ee1c81
Mocanu Alin <alin_ngt@yahoo.com>
2022-12-07 15:43:35 +0200

    Import csv with manual select file

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   StatusBar,
   FlatList,
+  Pressable,
 } from 'react-native';
 import XLSX from 'xlsx';
 import fs from 'react-native-fs';
@@ -15,40 +16,46 @@ import FlatItem from '../components/FlatItem';
 const SettingsScreen = ({navigation}) => {
   const [fileList, setFileList] = useState([]);
   const [incomeList, setIncomeList] = useState([]);
-
-  const printFs = async () => {
-    const tmp = await fs.readDir(fs.DownloadDirectoryPath);
-    const alin = [];
-    tmp.forEach(item => {
-      alin.push(item.path);
-      // console.log(item.path);
-    });
-    setFileList(alin);
-  };
+  const [fileToRead, setFileToRead] = useState('');
 
   useEffect(() => {
     console.log('Settings useEffect');
     printFs();
   }, []);
 
-  const moc1 = async () => {
-    console.log('====================================');
-    const alin2 = await fs.readFile(fileList[0]);
-    const alin1 = readString(alin2, {header: false, delimiter: ''});
+  const printFs = async () => {
+    const tmp = await fs.readDir(fs.DownloadDirectoryPath);
+    const alin = [];
+    tmp.forEach(item => {
+      if(!item.path.endsWith(".csv")) return
+      alin.push(item.path);
+    });
+    setFileList(alin);
+    if(alin.length > 0){
+      moc1(alin[0])
+    }
+  };
+
+  const moc1 = async (x) => {
+    const alin2 = await fs.readFile(x);
+    const alin1 = readString(alin2, {header: false, delimiter: ','});
     const temp = [];
-    alin1.data.forEach((item, index) => {
-      if(index === 0) return
+    const t = alin1.data
+    for(let i = 1; i < 20; i++){
       const obj = {
-        details: item[0],
-        dt: item[1],
-        description: item[2],
-        income: +item[3],
-        id: item[4],
+        details: t[i][0],
+        dt: t[i][1],
+        description: t[i][2],
+        income: +t[i][3],
+        id: t[i][4],
       };
       temp.push(obj);
-    });
-    console.log(temp);
+    }
     setIncomeList(temp);
+  };
+
+  const handleFileSelect = x => {
+    moc1(x)
   };
   return (
     <View style={styles.container}>
@@ -56,31 +63,40 @@ const SettingsScreen = ({navigation}) => {
       <View style={styles.topBar}>
         <View style={styles.buttonContainer}>
           <Button title="<-" onPress={() => navigation.navigate('Main')} />
-          <Button title="getFS" onPress={printFs} />
-          <Button title="moc1" onPress={moc1} />
+          {/* <Button title="getFS" onPress={printFs} /> */}
+          {/* <Button title="moc1" onPress={moc1} /> */}
         </View>
         <Text style={styles.barText}>Settings page</Text>
       </View>
       <View style={styles.main}>
         <FlatList
-          data={incomeList}
+          data={fileList}
           renderItem={({item, index}) => (
-            <View style={{borderWidth: 1, borderColor: '#ff000088'}}>
-              <Text
-                style={{
-                  color: 'white',
-                  width: '100%',
-                  textAlign: 'left',
-                  paddingLeft: 10,
-                }}>
-                {item.dt} - {item.income} - {item.id}
-              </Text>
-              <Text style={{color: 'coral', paddingLeft: 10}}>
-                {item.details}
-              </Text>
+            <View style={{borderWidth: 1, borderColor: '#ff000088', marginVertical: 10}}>
+              <Pressable onPress={() => handleFileSelect(item)}>
+                <Text style={{color: 'coral', padding: 10}}>
+                  {item.split('/')[item.split('/').length - 1]}
+                </Text>
+              </Pressable>
             </View>
           )}
-          keyExtractor={item => item.id}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+      <View style={styles.main}>
+        <Text style={{color: "white", width: "100%", textAlign: "center",borderColor: "green", borderWidth: 1}} >Preview first 3:</Text>
+        <FlatList
+          data={incomeList}
+          renderItem={({item}) => (
+            <View style={{borderWidth: 1, borderColor: '#ff000088', marginVertical: 10}}>
+              <Pressable onPress={() => handleFileSelect(item)}>
+                <Text style={{color: 'coral', padding: 10}}>
+                  {item.description} - {item.dt}
+                </Text>
+              </Pressable>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
         />
       </View>
     </View>
@@ -113,9 +129,9 @@ const styles = StyleSheet.create({
   },
   main: {
     width: '100%',
-    // borderWidth: 1,
-    // borderColor: 'red',
-    flex: 1,
+    borderWidth: 1,
+    borderColor: 'red',
+    // flex: 1,
   },
 });
 
